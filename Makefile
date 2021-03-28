f61d346fecb914465c7ed460eee6e090216aa390
NastyBoget <bogatenkova.anastasiya@mail.ru>
2021-03-28 17:40:22 +0300

    marked and worklist structures added

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
+all: dce
+dce: dce.cpp
+	g++ dce.cpp -L/usr/bin/lib -lqbe -o dce
 test:
 	bash run.sh
 clean:
 	rm -rf dce
 	rm -R outputs
-run : 
+run: dce
 	./dce
--- dce.cpp
+++ dce.cpp
@@ -1,28 +1,51 @@
-#ifdef __cplusplus
 #define export exports
 extern "C" {
 #include <qbe/all.h>
 }
 #undef export
-#else
-#include <qbe/all.h>
-#endif
 
 #include <stdio.h>
+#include <string>
+#include <set>
+
+using namespace std;
+
+// множество помеченных инструкций трех типов:
+// 1) инструкции, описываемые структурой Ins (не являющиеся терминаторами) (blk->ins)
+// 2) инструкции-терминаторы (безусловный переход, ветвление и возврат из функции) (blk->jmp)
+// 3) фи-функции (blk->phi)
+// элемент множества - пара <имя блока, номер инструкции в блоке>
+// имя блока: blk->name
+// номер инструкции: неотрицательное число для инструкций первого типа (индекс в массиве blk->ins),
+// для инструкций-терминаторов номер равен -1 (в каждом блоке не более 1 перехода)
+// для фи-функций нумерация от -2 в обратную сторону (номер фи-функции в списке blk->phi начиная с -2) -2, -3, ...
+set<pair<string, int>> marked_instructions;
+
+// множество, аналогичное множеству помеченных инструкций
+// используется на шаге mark алгоритма
+set<pair<string, int>> work_list;
+
+// множество помеченных блоков программы для вычисления ближайшего помеченного постдоминатора
+set<string> marked_blocks;
+
+// заполнение множеств marked_instructions и work_list критическими инструкциями
+// 1) инструкции, возвращающие значение из процедуры
+// 2) инструкции ввода-вывода
+// 3) инструкции, влияющие на значения в доступных извне процедуры областях памяти
+static void find_critical_instructions(Fn *fn) {}
 
 static int useful(Ins* i) {
-
+    return 0;
 }
 
 static void readfn (Fn *fn) {
-    fillrpo(fn); // Traverses the CFG in reverse post-order, filling blk->id.
+    fillrpo(fn);
     fillpreds(fn);
     filluse(fn);
     ssa(fn);
 
-    
     for (Blk *blk = fn->start; blk; blk = blk->link) {
-        for (Ins *i = blk->ins; i < &blk->ins[b->nins]; ++i) {
+        for (Ins *i = blk->ins; i < &blk->ins[blk->nins]; ++i) {
             if (!useful(i)) {
                 i->op = Onop;
                 i->to = R;
@@ -33,8 +56,8 @@ static void readfn (Fn *fn) {
         
     }
 
-    fillpreds(fn); // Either call this, or keep track of preds manually when rewriting branches.
-    fillrpo(fn); // As a side effect, fillrpo removes any unreachable blocks.
+    fillpreds(fn);
+    fillrpo(fn);
     printfn(fn, stdout);
 }
 
@@ -43,6 +66,6 @@ static void readdat (Dat *dat) {
 }
 
 int main () {
-  parse(stdin, "<stdin>", readdat, readfn);
+  parse(stdin, (char *)"<stdin>", readdat, readfn);
   freeall();
 }
