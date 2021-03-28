4cb2979cd4d534cf0573f3d0a84a263031881dba
NastyBoget <bogatenkova.anastasiya@mail.ru>
2021-03-28 15:00:40 +0300

    initial commit

---
+++ Makefile
@@ -0,0 +1,7 @@
+test:
+	bash run.sh
+clean:
+	rm -rf dce
+	rm -R outputs
+run : 
+	./dce
---
+++ dce.cpp
@@ -0,0 +1,48 @@
+#ifdef __cplusplus
+#define export exports
+extern "C" {
+#include <qbe/all.h>
+}
+#undef export
+#else
+#include <qbe/all.h>
+#endif
+
+#include <stdio.h>
+
+static int useful(Ins* i) {
+
+}
+
+static void readfn (Fn *fn) {
+    fillrpo(fn); // Traverses the CFG in reverse post-order, filling blk->id.
+    fillpreds(fn);
+    filluse(fn);
+    ssa(fn);
+
+    
+    for (Blk *blk = fn->start; blk; blk = blk->link) {
+        for (Ins *i = blk->ins; i < &blk->ins[b->nins]; ++i) {
+            if (!useful(i)) {
+                i->op = Onop;
+                i->to = R;
+                i->arg[0] = R;
+                i->arg[1] = R;
+            }
+        }
+        
+    }
+
+    fillpreds(fn); // Either call this, or keep track of preds manually when rewriting branches.
+    fillrpo(fn); // As a side effect, fillrpo removes any unreachable blocks.
+    printfn(fn, stdout);
+}
+
+static void readdat (Dat *dat) {
+  (void) dat;
+}
+
+int main () {
+  parse(stdin, "<stdin>", readdat, readfn);
+  freeall();
+}
