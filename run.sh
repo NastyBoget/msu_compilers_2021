853223ef2b7ac79278648be66c8b2ace64a11a46
Max Raenchuk <raenchuk@ispras.ru>
2021-04-01 13:36:39 +0300

    Add test with phi function

--- run.sh
+++ run.sh
@@ -4,3 +4,4 @@ g++ dce.cpp -L./lib -lqbe -I ./include -o dce
 ./dce < tests/1.txt > outputs/1.txt
 ./dce < tests/2.txt > outputs/2.txt
 ./dce < tests/3.txt > outputs/3.txt
+./dce < tests/4.txt > outputs/4.txt
